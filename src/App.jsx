14eabcd096d44d8e595778bc06c4a089df30a876
Oussama <saidi.oussama2004@gmail.com>
2025-03-28 18:00:42 +0000

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import SearchBar from "./components/SearchBar";
 import axios from "axios";
 import WeatherCard from "./components/WeatherCard";
+import video from "./video.mp4";
 
 function App() {
   const [weather, setWeather] = useState(null);
@@ -36,7 +37,17 @@ function App() {
       className="min-h-screen flex flex-col items-center justify-center
      bg-blue-100"
     >
-      <div className="bg-black/90 text-white rounded-lg shadow-lg p-8 max-w-md w-full">
+      <video
+        className="absolute top-0 left-0 w-full h-full object-cover"
+        autoPlay
+        loop
+        muted
+      >
+        <source src={video} type="video/mp4" />
+        Your browser does not support the video tag
+      </video>
+      <div className="absolute top-0 left-0 w-full h-full bg-black/40 z-1"></div>
+      <div className="bg-black/75 text-white rounded-lg shadow-lg p-8 max-w-md w-full z-1000">
         <h1 className="text-3xl font-bold text-center mb-6">Weather App</h1>
         <SearchBar fetchWeather={fetchWeather} />
         {loading && (
